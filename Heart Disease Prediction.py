122ee2495717342fb99db71789ba2e508f268218
Srihesh Kothapalli <146610674+Srihesh@users.noreply.github.com>
2024-08-26 20:38:54 +0530

    Add files via upload

---
+++ Heart Disease Prediction.py
@@ -0,0 +1,71 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import rcParams
+from matplotlib.cm import rainbow
+import warnings
+warnings.filterwarnings('ignore')
+
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+
+from sklearn.neighbors import KNeighborsClassifier
+
+
+dataset = pd.read_csv('dataset.csv')
+dataset.info()
+
+
+dataset.describe()
+
+
+rcParams['figure.figsize'] = 20, 14
+plt.matshow(dataset.corr())
+plt.yticks(np.arange(dataset.shape[1]), dataset.columns)
+plt.xticks(np.arange(dataset.shape[1]), dataset.columns)
+plt.colorbar()
+
+
+dataset.hist()
+
+rcParams['figure.figsize'] = 8,6
+plt.bar(dataset['target'].unique(), dataset['target'].value_counts(), color = ['red', 'green'])
+plt.xticks([0, 1])
+plt.xlabel('Target Classes')
+plt.ylabel('Count')
+plt.title('Count of each Target Class')
+
+
+dataset = pd.get_dummies(dataset, columns = ['sex', 'cp', 'fbs', 'restecg', 'exang', 'slope', 'ca', 'thal'])
+
+
+standardScaler = StandardScaler()
+columns_to_scale = ['age', 'trestbps', 'chol', 'thalach', 'oldpeak']
+dataset[columns_to_scale] = standardScaler.fit_transform(dataset[columns_to_scale])
+
+
+y = dataset['target']
+X = dataset.drop(['target'], axis = 1)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.33, random_state = 0)
+
+
+knn_scores = []
+for k in range(1,21):
+    knn_classifier = KNeighborsClassifier(n_neighbors = k)
+    knn_classifier.fit(X_train, y_train)
+    knn_scores.append(knn_classifier.score(X_test, y_test))
+
+plt.plot([k for k in range(1, 21)], knn_scores, color = 'red')
+for i in range(1,21):
+    plt.text(i, knn_scores[i-1], (i, knn_scores[i-1]))
+plt.xticks([i for i in range(1, 21)])
+plt.xlabel('Number of Neighbors (K)')
+plt.ylabel('Scores')
+plt.title('K Neighbors Classifier scores for different K values')
+
+print("The score for K Neighbors Classifier is {}% with {} nieghbors.".format(knn_scores[7]*100, 8))
+
+
+
